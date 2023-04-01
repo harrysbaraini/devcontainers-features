5be1c0a3836419670682034832b1cb534340a594
Vanderlei Sbaraini Amancio <hello@vanderleis.me>
2023-04-01 20:52:27 -0300

    Add php-cli and nginx-unit-php

---
+++ src/nginx-unit-php/install.sh
@@ -0,0 +1,120 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'nginx-unit-php'"
+
+PORT=${PORT:-'80'}
+APP_ROOT=${APP_ROOT:-'/var/www/html/public'}
+echo "The provided port is: $PORT"
+echo "The provided app root path is: $APP_ROOT"
+
+# The 'install.sh' entrypoint script is always executed as the root user.
+#
+# These following environment variables are passed in by the dev container CLI.
+# These may be useful in instances where the context of the final
+# remoteUser or containerUser is useful.
+# For more details, see https://containers.dev/implementors/features#user-env-var
+echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+
+# Add required apt repositories
+curl --output /usr/share/keyrings/nginx-keyring.gpg https://unit.nginx.org/keys/nginx-keyring.gpg \
+    && echo "deb [signed-by=/usr/share/keyrings/nginx-keyring.gpg] https://packages.nginx.org/unit/ubuntu/ jammy unit" > /etc/apt/sources.list.d/unit.list \
+    && echo "deb-src [signed-by=/usr/share/keyrings/nginx-keyring.gpg] https://packages.nginx.org/unit/ubuntu/ jammy unit" >> /etc/apt/sources.list.d/unit.list
+
+apt-get update
+apt-get -y --no-install-recommends install \
+    && unit \
+    && unit-php
+
+# Create the config.json
+mkdir /nginx-unit
+
+cat << EOF > /nginx-unit/config.json
+{
+    "listeners": {
+        "*:80": {
+            "pass": "routes"
+        }
+    },
+
+    "routes": [
+        {
+            "match": {
+                "uri": "!/index.php"
+            },
+            "action": {
+                "share": "${APP_ROOT}\$uri",
+                "fallback": {
+                    "pass": "applications/php"
+                }
+            }
+        }
+    ],
+
+    "applications": {
+        "php": {
+            "type": "php",
+            "root": "${APP_ROOT}/",
+            "script": "index.php"
+        }
+    }
+}
+EOF
+
+# Create the entrypoint
+cat << EOF > /usr/local/bin/nginx-unit.sh
+#!/bin/bash
+
+set -e
+WAITLOOPS=5
+SLEEPSEC=1
+
+curl_put()
+{
+    RET=\$(/usr/bin/curl -s -w '%{http_code}' -X PUT --data-binary @\$1 --unix-socket /var/run/control.unit.sock http://localhost/\$2)
+    RET_BODY=\$(echo \$RET | /bin/sed '\$ s/...\$//')
+    RET_STATUS=\$(echo \$RET | /usr/bin/tail -c 4)
+    if [ "\$RET_STATUS" -ne "200" ]; then
+        echo "\$0: Error: HTTP response status code is '\$RET_STATUS'"
+        echo "\$RET_BODY"
+        return 1
+    else
+        echo "\$0: OK: HTTP response status code is '\$RET_STATUS'"
+        echo "\$RET_BODY"
+    fi
+    return 0
+}
+
+if [ "\$1" = "unitd" ] || [ "\$1" = "unitd-debug" ]; then
+    /usr/sbin/\$1 --control unix:/var/run/control.unit.sock
+
+    for i in \$(/usr/bin/seq \$WAITLOOPS); do
+        if [ ! -S /var/run/control.unit.sock ]; then
+            echo "\$0: Waiting for control socket to be created..."
+            /bin/sleep \$SLEEPSEC
+        else
+            break
+        fi
+    done
+
+    # even when the control socket exists, it does not mean unit has finished initialisation
+    # this curl call will get a reply once unit is fully launched
+    /usr/bin/curl -s -X GET --unix-socket /var/run/control.unit.sock http://localhost/
+
+    echo "\$0: Apply configuration"
+    curl_put "/nginx-unit/config.json" "config"
+
+    echo
+    echo "\$0: Unit initial configuration complete; Nginx Unit ready."
+    echo
+fi
+
+exec "$@"
+EOF
+
+# clean up
+rm -rf /var/lib/apt/lists/*
---
+++ src/php-cli/install.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'php-cli'"
+
+VERSION=${VERSION:-'8.2'}
+PACKAGES=${PACKAGES:-''}
+TIMEZONE=${TIMEZONE:-'UTC'}
+echo "The provided version is: $VERSION"
+echo "The provided packages are: $PACKAGES"
+echo "The provided timezone is: $TIMEZONE"
+
+# The 'install.sh' entrypoint script is always executed as the root user.
+#
+# These following environment variables are passed in by the dev container CLI.
+# These may be useful in instances where the context of the final
+# remoteUser or containerUser is useful.
+# For more details, see https://containers.dev/implementors/features#user-env-var
+echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+
+# Environment variables
+PHP_DATE_TIMEZONE="${TIMEZONE}"
+PHP_ERROR_REPORTING="22527"
+PHP_MEMORY_LIMIT="256M"
+PHP_MAX_EXECUTION_TIME="99"
+PHP_POST_MAX_SIZE="100M"
+PHP_UPLOAD_MAX_FILE_SIZE="100M"
+COMPOSER_ALLOW_SUPERUSER=1
+COMPOSER_HOME=/composer
+COMPOSER_MAX_PARALLEL_HTTP=24
+
+# timezone
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# install system dependencies
+apt-get update
+apt-get -y --no-install-recommends install \
+    software-properties-common \
+    ca-certificates \
+    curl \
+    unzip \
+    gnupg2
+
+# Add required repository
+add-apt-repository -y ppa:ondrej/php
+
+# Install PHP
+apt-get update
+
+# Install Nginx Unit and PHP extensions
+if [ -z "${PACKAGES}" ]
+packagesArr=(${PACKAGES})
+
+for pkg in "${packagesArr[@]}"
+do
+   apt-get -y --no-install-recommends install php${PHP_VERSION}-${pkg}
+done
+
+apt-get -y --no-install-recommends install \
+    php${PHP_VERSION}-cli \
+    php${PHP_VERSION}-common \
+    php${PHP_VERSION}-igbinary \
+    php${PHP_VERSION}-readline \
+    php${PHP_VERSION}-curl \
+    php${PHP_VERSION}-intl \
+    php${PHP_VERSION}-curl \
+    php${PHP_VERSION}-tokenizer \
+    php${PHP_VERSION}-mbstring \
+    php${PHP_VERSION}-bcmath \
+    php${PHP_VERSION}-xml \
+    php${PHP_VERSION}-zip \
+    php${PHP_VERSION}-sqlite3
+
+set +e
+    PHP_SRC=$(which php)
+set -e
+
+# install composer
+"${PHP_SRC}" -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+"${PHP_SRC}" -r "if (hash_file('sha384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+"${PHP_SRC}" composer-setup.php --install-dir="/usr/local/bin" --filename=composer
+"${PHP_SRC}" -r "unlink('composer-setup.php');"
+
+# clean up
+rm -rf /var/lib/apt/lists/*
