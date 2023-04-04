d4583efa52b5b02a0a571c2f62ef3312a1d0ab4d
Vanderlei Sbaraini Amancio <hello@vanderleis.me>
2023-04-03 22:34:09 -0300

    revert unit

--- src/nginx-unit-php/install.sh
+++ src/nginx-unit-php/install.sh
@@ -22,72 +22,18 @@ echo "The effective dev container containerUser's home directory is '$_CONTAINER
 export DEBIAN_FRONTEND=noninteractive
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 
-mkdir /nginx-unit
-
-# Compile Unit
+# Add required apt repositories
+curl --output /usr/share/keyrings/nginx-keyring.gpg https://unit.nginx.org/keys/nginx-keyring.gpg \
+    && echo "deb [signed-by=/usr/share/keyrings/nginx-keyring.gpg] https://packages.nginx.org/unit/ubuntu/ jammy unit" > /etc/apt/sources.list.d/unit.list \
+    && echo "deb-src [signed-by=/usr/share/keyrings/nginx-keyring.gpg] https://packages.nginx.org/unit/ubuntu/ jammy unit" >> /etc/apt/sources.list.d/unit.list
 
 apt-get update
-apt-get install --no-install-recommends --no-install-suggests -y ca-certificates mercurial build-essential libssl-dev libpcre2-dev
-mkdir -p /usr/lib/unit/modules /usr/lib/unit/debug-modules
-hg clone https://hg.nginx.org/unit
-cd unit
-hg up 1.29.1
-NCPU="$(getconf _NPROCESSORS_ONLN)"
-DEB_HOST_MULTIARCH="$(dpkg-architecture -q DEB_HOST_MULTIARCH)"
-CC_OPT="$(DEB_BUILD_MAINT_OPTIONS="hardening=+all,-pie" DEB_CFLAGS_MAINT_APPEND="-Wp,-D_FORTIFY_SOURCE=2 -fPIC" dpkg-buildflags --get CFLAGS)"
-LD_OPT="$(DEB_BUILD_MAINT_OPTIONS="hardening=+all,-pie" DEB_LDFLAGS_MAINT_APPEND="-Wl,--as-needed -pie" dpkg-buildflags --get LDFLAGS)"
-CONFIGURE_ARGS="--prefix=/usr \
-                --state=/var/lib/unit \
-                --control=unix:/var/run/control.unit.sock \
-                --pid=/var/run/unit.pid \
-                --log=/var/log/unit.log \
-                --tmp=/var/tmp \
-                --user=unit \
-                --group=unit \
-                --openssl \
-                --libdir=/usr/lib/$DEB_HOST_MULTIARCH"
-
-./configure $CONFIGURE_ARGS --cc-opt="$CC_OPT" --ld-opt="$LD_OPT" --modules=/usr/lib/unit/debug-modules --debug
-make -j $NCPU unitd
-install -pm755 build/unitd /usr/sbin/unitd-debug
-make clean
-./configure $CONFIGURE_ARGS --cc-opt="$CC_OPT" --ld-opt="$LD_OPT" --modules=/usr/lib/unit/modules
-make -j $NCPU unitd
-install -pm755 build/unitd /usr/sbin/unitd
-make clean
-./configure $CONFIGURE_ARGS --cc-opt="$CC_OPT" --modules=/usr/lib/unit/debug-modules --debug
-./configure php
-make -j $NCPU php-install
-make clean
-./configure $CONFIGURE_ARGS --cc-opt="$CC_OPT" --modules=/usr/lib/unit/modules
-./configure php
-make -j $NCPU php-install
-ldd /usr/sbin/unitd | awk '/=>/{print $(NF-1)}' | while read n; do dpkg-query -S $n; done | sed 's/^\([^:]\+\):.*$/\1/' | sort | uniq > /nginx-unit/requirements.apt
-ldconfig
-
-if [ -f "/tmp/libunit.a" ]; then \
-    mv /tmp/libunit.a /usr/lib/$(dpkg-architecture -q DEB_HOST_MULTIARCH)/libunit.a; \
-    rm -f /tmp/libunit.a; \
-fi
-
-mkdir -p /var/lib/unit/
-addgroup --system unit
-adduser \
-    --system \
-    --disabled-login \
-    --ingroup unit \
-    --no-create-home \
-    --home /nonexistent \
-    --gecos "unit user" \
-    --shell /bin/false \
-    unit
-
-apt update
-apt --no-install-recommends --no-install-suggests -y install curl $(cat /nginx-unit/requirements.apt)
-rm /nginx-unit/requirements.apt
-apt-get clean
+apt-get -yq --no-install-recommends install \
+    unit \
+    unit-php
 
 # Create the entrypoint
+mkdir /nginx-unit
 
 cat << EOFFILE > /usr/local/bin/nginx-unit.sh
 #!/bin/bash
@@ -179,5 +125,4 @@ EOFFILE
 chmod a+x /usr/local/bin/nginx-unit.sh
 
 # clean up
-
 rm -rf /var/lib/apt/lists/*
